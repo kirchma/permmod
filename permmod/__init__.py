c3a3042e0f66c5a738ef8b0f0ff4ec2a7088c20b
kirchma <mkirch@web.de>
2023-03-13 16:15:03 +0100

    Add MeasurementReaktor Class and example files

--- permmod/__init__.py
+++ permmod/__init__.py
@@ -1,4 +1,5 @@
 from .measurement import Measurement
+from .measurement import MeasurementReaktor
 from .import_data import Data
 from .linear_system import LinearSystem
 from .optimize import Optimizer
